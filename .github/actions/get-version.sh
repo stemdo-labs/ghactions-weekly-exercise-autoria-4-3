ae247d04c17a86a9ed31d0fc09965da215358ee8
icasado01 <166407751+icasado01@users.noreply.github.com>
2024-05-03 14:41:53 +0200

    Update get-version.sh

--- .github/actions/get-version.sh
+++ .github/actions/get-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Lee el archivo de configuración y extrae la versión
-version=$(jq -r '.version' package.json)
+version=$(grep -Po '"version": "\K[^"]+' package.json)
 
 # Establece la versión como salida de la acción
 echo "::set-output name=version::$version"
